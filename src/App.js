51842dc2f099c84cbddc924c7243b16cdc0a03b1
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-10-07 19:29:43 -0400

    added a component dictionary, added a search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import Dictionary from './dictionary';
 import logo from "./dictionary.png";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
        <img src={logo} className="App-logo" Alt="logo" />
-      
       </header>
+      <main>
+        <Dictionary />
+      </main>
+    </div>
     </div>
   );
 }
