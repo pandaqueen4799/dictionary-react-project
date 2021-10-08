e2e2fd333b35f41e656c2de8b4bf5020375aea91
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-10-07 22:08:59 -0400

    added dictionary api url https://dictionaryapi.dev/
    https://dictionaryapi.dev/

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-       <img src={logo} className="App-logo" Alt="logo" />
+       <img src={logo} className="App-logo" alt="logo" />
       </header>
       <main>
         <Dictionary />
