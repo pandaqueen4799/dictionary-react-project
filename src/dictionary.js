0a111cfeb5c00a9651a7d7bdf0253afc1f61f320
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-10-08 20:40:19 -0400

    added a result component
    not working

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./dictionary.css";
+import Results from "./results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    
 
+    // documentation:https://dictionaryapi.dev/ `https://api.dictionaryapi.dev/api/v2/entries/en/`
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/
-    en/${keyword}`;
+    %20%20%20%20en/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
@@ -28,6 +31,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/results.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Results(props) {
+    if (props.results) {
+    return  <div className="results">
+          hello from results
+        </div>;
+        } else {
+            return null;
+        }
+}
