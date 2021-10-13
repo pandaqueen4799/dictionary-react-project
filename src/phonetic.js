3871c14d94d9f57794b44936f60527897d2f6ae1
pandaqueen4799 <58620160+pandaqueen4799@users.noreply.github.com>
2021-10-13 14:24:28 -0400

    fixed results phonetic and added the voice audio

--- src/phonetic.js
+++ src/phonetic.js
@@ -2,5 +2,13 @@ import React from "react";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-    return  "hello from phonetic";
+    return (
+      <div className="phonetic">
+        <a href={props.phonetic.audio} target="_blank">
+        Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
+    );
 }
